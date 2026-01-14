57c5c0abb621619644c7a82c8222d3846722d50f
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-01-14 16:28:33 -0300

    centralizar vertical

--- src/App.js
+++ src/App.js
@@ -11,23 +11,41 @@ import NavBar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
 
 function App() {
+
   return (
+
     <Router>
-      <NavBar />
 
-      <Container customClass="min-height">
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/projects" element={<Projects />}/>
-          <Route path="/company" element={<Company />}/>
-          <Route path="/contact" element={<Contact />}/>
-          <Route path="/newproject" element={<NewProject />} />
-        </Routes>
-      </Container>
+      <div className="router_wrapper">
 
-      <Footer />
+        <NavBar />
+
+        <Container customClass="min-height">
+
+          <Routes>
+
+            <Route path="/" element={<Home />}/>
+
+            <Route path="/projects" element={<Projects />}/>
+
+            <Route path="/company" element={<Company />}/>
+
+            <Route path="/contact" element={<Contact />}/>
+
+            <Route path="/newproject" element={<NewProject />} />
+
+          </Routes>
+
+        </Container>
+
+        <Footer />
+
+      </div>
+
     </Router>
+
   );
+
 }
 
 export default App;
--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -5,9 +5,11 @@ function NewProject() {
 
     return(
         <div className={styles.newproject_container}>
-            <h1>Criar Projeto</h1>
-            <p>Crie seu projet para começar a planejar seu evento. Após a criação, você poderá gerenciar todos os seus serviços.</p>
-            <ProjectForm btnText="Criar     Projeto" />
+            <div className={styles.project_content}>
+                <h1>Criar Projeto</h1>
+                <p>Crie seu projet para começar a planejar seu evento. Após a criação, você poderá gerenciar todos os seus serviços.</p>
+                <ProjectForm btnText="Criar     Projeto" />
+            </div>
         </div>
     )
 }
