1207229bc35e1ddf1c47f6649e50f0f617618d40
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-01-15 16:05:34 -0300

    get api

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react"
+
 import styles from "./ProjectForm.module.css"
 import Input from "../form/Input"
 import Select from "../form/Select"
@@ -6,11 +8,26 @@ import { FaPlusCircle } from "react-icons/fa"
 
 function ProjectForm({btnText}) {
 
+    const [ categories, setCategories ] = useState([])
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                setCategories(data)
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
             <Input type="number" text="Orçamento" name="budget" placeholder="Insira o orçamento total" />
-            <Select name="category_id" text="Selecione a categoria" />
+            <Select name="category_id" text="Selecione a categoria" option={categories}/>
             <SubmitButton text={btnText} icon={FaPlusCircle}/>
         </form>
     )
