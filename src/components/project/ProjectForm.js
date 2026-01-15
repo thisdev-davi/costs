01b3586b15407c0a9ccf58ef7124d285fec22e95
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-01-15 16:10:27 -0300

    correcao options

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -27,7 +27,7 @@ function ProjectForm({btnText}) {
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto" />
             <Input type="number" text="Orçamento" name="budget" placeholder="Insira o orçamento total" />
-            <Select name="category_id" text="Selecione a categoria" option={categories}/>
+            <Select name="category_id" text="Selecione a categoria" options={categories}/>
             <SubmitButton text={btnText} icon={FaPlusCircle}/>
         </form>
     )
