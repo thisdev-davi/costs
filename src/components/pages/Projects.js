20984ec7bfab7585dee452903316d4b4353f6f69
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-01-21 16:33:38 -0300

    messages

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,12 +1,18 @@
-import { useEffectEvent, useState } from "react"
 import Message from "../layout/Message"
+import { useLocation } from "react-router-dom"
 
 function Projects() {
+
+    const location = useLocation()
+    let message = ""
+    if(location.state){
+        message = location.state.message
+    }
 
     return(
         <div>
             <h1>Meus Projetos</h1>
-            <Message msg={"Alguma mensagem"} />
+            {message && <Message msg={message} type="success"/>}
         </div>
     )
 }
