3ed91effc5f424a95a15cbc2f4ecefff5ee47b52
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-01-21 18:18:18 -0300

    link button page projects

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,10 @@
 import Message from "../layout/Message"
 import { useLocation } from "react-router-dom"
+import Container from "../layout/Container"
+import LinkButton from "../layout/LinkButton"
+
+import styles from "./Projects.module.css"
+import { FaArrowCircleRight } from "react-icons/fa"
 
 function Projects() {
 
@@ -10,9 +15,15 @@ function Projects() {
     }
 
     return(
-        <div>
-            <h1>Meus Projetos</h1>
-            {message && <Message msg={message} type="success"/>}
+        <div className={styles.projects_container}>
+            <div className={styles.title_container}>
+                <h1>Meus Projetos</h1>
+                <LinkButton to="/newproject" text="Criar Projeto" icon={FaArrowCircleRight}/>
+            </div>
+                {message && <Message msg={message} type="success"/>}
+                <Container customClass="start">
+                    <p>Projetos...</p>
+                </Container>
         </div>
     )
 }
