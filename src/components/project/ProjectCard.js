7430b66e4e8eeb99a8af6cdcf8a2d5b45592a1eb
thisdev-davi <davisouzaoliveira1975@gmail.com>
2026-01-21 23:37:26 -0300

    project card

---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,28 @@
+import styles from "./ProjectCard.module.css"
+import { Link } from "react-router-dom"
+import { FaPencilAlt, FaTrash } from "react-icons/fa"
+
+function ProjectCard({ id, name, budget, category, handleRemove }) {
+
+    return (
+        <div className={styles.project_card}>
+            <h4>{name}</h4>
+            <p>
+                <span>Orçamento:</span> R${budget}
+            </p>
+            <p className={styles.category_text}>
+                <span className={`${styles[category.toLowerCase()]}`}></span> {category}
+            </p>
+            <div className={styles.project_card_actions}>
+                <Link>
+                    <FaPencilAlt /> Editar
+                </Link>
+                <button>
+                    <FaTrash /> Excluir
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default ProjectCard
